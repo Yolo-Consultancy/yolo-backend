cda8043a0b71c8442820c50e99db9ef117dc428a
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2026-06-11 11:41:27 +0100

    feat : Initial commit with project setup and basic structure

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const { success } = require('zod');
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ 
+    success: true,
+    message: 'Yolo Backend is running!' 
+  });
+});
+
+module.exports = app;
